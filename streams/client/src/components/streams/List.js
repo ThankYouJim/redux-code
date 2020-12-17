d4500bf0afbad8e40afe5e46b85128e3507946d3
Jo <jo.chong@mav3rik.com>
2020-12-17 18:25:14 +1100

    add files

---
+++ streams/client/src/components/streams/List.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { connect } from "react-redux";
+import { fetchStreams } from "../../actions";
+
+class List extends React.Component {
+  componentDidMount() {
+    this.props.fetchStreams();
+  }
+
+  renderList() {
+    return this.props.streams.map(stream => (
+      <div className="item" key={stream.id}>
+        <i className="large middle aligned icon camera" />
+        <div className="content">
+          {stream.title}
+          <div className="description">{stream.description}</div>
+        </div>
+      </div>
+    ));
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Streams</h2>
+        {this.renderList()}
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return { streams: Object(state.streams) };
+};
+
+export default connect(mapStateToProps, { fetchStreams })(List);
